bac345d175fdf4052e50c67f0fbf1cf3ca8a4110
ben <ben@dreamerslab.com>
2012-02-08 00:13:33 +0800

    use object.jey instead of for in

--- test/run.js
+++ test/run.js
@@ -2,11 +2,13 @@ var tests = require( './inflection' );
 var Flow  = require( 'node.flow' );
 var flow  = new Flow();
 
-for( var test in tests ){
+
+
+Object.keys( tests ).forEach( function( test ){
   flow.parallel( function ( ready ){
     tests[ test ]( ready );
   });
-}
+});
 
 flow.join().end( function (){
   console.log( 'All test passed :)' );
